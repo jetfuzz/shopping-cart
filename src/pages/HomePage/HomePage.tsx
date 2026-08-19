a02e4147e92e0d84d248937f99ee76a4975ce510
jetfuzz <1jsf23@gmail.com>
2026-08-19 08:41:18 -0500

    feat: add featured products to HomePage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,34 @@
-import { Link } from 'react-router';
+import { Link, useOutletContext } from 'react-router';
 import styles from './HomePage.module.css';
+import type { Product } from '../../types';
+import ItemCard from '../../components/ItemCard/ItemCard';
+
+interface HomePageContext {
+  products: Product[];
+}
+
+function getFeaturedProducts(products: Product[]): Product[] {
+  const seenCategories = new Set<string>();
+  const featured: Product[] = [];
+  const MIN_FEATURED_RATING = 4;
+
+  for (const p of products) {
+    if (
+      !seenCategories.has(p.category) &&
+      p.rating.rate > MIN_FEATURED_RATING
+    ) {
+      featured.push(p);
+      seenCategories.add(p.category);
+    }
+  }
+
+  return featured;
+}
 
 export default function HomePage() {
+  const { products } = useOutletContext<HomePageContext>();
+  const featured = getFeaturedProducts(products);
+
   return (
     <main className={styles.homePage}>
       <section className={styles.hero}>
@@ -12,7 +39,14 @@ export default function HomePage() {
         </Link>
       </section>
 
-      <section className={styles.featured}></section>
+      <section>
+        <h3>Featured Products</h3>
+        <div className={styles.products}>
+          {featured.map((p) => (
+            <ItemCard key={p.id} product={p} />
+          ))}
+        </div>
+      </section>
     </main>
   );
 }
